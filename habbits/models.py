c793ab475cf49bb6ce22fcbfa7dc45dfb338803b
tmitev <todor.mitev@sofairo.com>
2026-01-21 14:10:26 +0300

    corrections 1.0

--- habbits/models.py
+++ habbits/models.py
@@ -44,7 +44,11 @@ class Habbit(models.Model):
     )
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    user = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE, blank=True, null=True)
+
+    def __str__(self):
+        return f'{self.user}, {self.action}'
+
 
     class Meta:
         verbose_name = "Привычка"
--- users/models.py
+++ users/models.py
@@ -3,8 +3,20 @@ from django.db import models
 
 
 class User(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True, verbose_name="Электронная почта")
+    password = models.CharField(max_length=100, verbose_name='Пароль пользователя')
     telegram_id = models.CharField(
         max_length=100, unique=True, help_text="Введите свой chat id"
     )
 
     REQUIRED_FIELDS = []
+
+
+def __str__(self):
+    return f'{self.username}'
+
+
+class Meta:
+    verbose_name = 'Пользователь'
+    verbose_name_plural = 'Пользователи'
